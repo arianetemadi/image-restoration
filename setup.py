a4364eb7fe128dd6ca76921964dce4b165c4f7bf
Arian ETEMADIHAGHIGHI <arian.etemadihaghighi@ist.ac.at>
2024-12-19 17:33:14 +0100

    Data loading added.

--- setup.py
+++ setup.py
@@ -7,6 +7,10 @@ setup(
     license="MIT",
     install_requires=[
         "ipykernel==6.29.5",
+        "gdown==5.2.0",
+        "pysocks==1.7.1",
+        "matplotlib==3.10.0",
+        "tensorflow==2.18.0",
     ],
     packages=find_packages(),
     classifiers=[
